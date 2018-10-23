6f0cc04da2f5496b24a98f798e350701927161b8
abbasikov <writeumair@gmail.com>
2018-10-22 22:14:54 -0700

    modified index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+//Create a new component. This component should produce some HTML
 class App extends Component {
   constructor(props) {
     super(props);
@@ -9,10 +10,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        Hello there. React in a modern way!
+        React in a modern way.
       </div>
     );
   }
 }
 
+//Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector(".container"));
