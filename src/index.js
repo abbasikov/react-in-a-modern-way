10fbe189fd596fc0d898e598570ea5b7bf523a4a
abbasikov <writeumair@gmail.com>
2018-10-22 22:23:08 -0700

    removed first component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-
 //Create a new component. This component should produce some HTML
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        React in a modern way.
-      </div>
-    );
-  }
-}
 
 //Take this component's generated HTML and put it on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector(".container"));
