d2bad387a63f61459189a17c6418afe1c0b7959f
abbasikov <writeumair@gmail.com>
2018-10-22 22:03:51 -0700

    removed default components

--- src/index.js
+++ src/index.js
@@ -1,46 +1,15 @@
-import _ from "lodash";
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import YTSearch from "youtube-api-search";
-import SearchBar from "./components/search_bar";
-import VideoList from "./components/video_list";
-import VideoDetail from "./components/video_detail";
-const API_KEY = "AIzaSyBqWKXi-ciSoQ3VbqYoeZMVWfyx2NXluGQ";
 
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      videos: [],
-      selectedVideo: null
-    };
-
-    this.videoSearch("painting");
-  }
-
-  videoSearch(term) {
-    YTSearch({ key: API_KEY, term: term }, videos => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0]
-      });
-    });
   }
 
   render() {
-    const videoSearch = _.debounce(term => {
-      this.videoSearch(term);
-    }, 300);
-
     return (
       <div>
-        <SearchBar onSearchTermChange={videoSearch} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
-          videos={this.state.videos}
-        />
+        Hello there. React in a modern way!
       </div>
     );
   }
